869bfbc2f858fb8e727bb31209da69371d5e7fb7
Rachit Narayan <53686465+narayanrachit@users.noreply.github.com>
2020-06-02 02:30:26 +0530

    Add files via upload

---
+++ includes/common.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "store")or die($mysqli_error($con));
+session_start();
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>	
+        </center>
+    </div>
+</footer>
